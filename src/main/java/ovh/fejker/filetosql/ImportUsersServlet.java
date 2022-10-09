b0426e26d799723342ecedefffcd969080469760
Oskar Migowski <o.migowski@protonmail.com>
2022-10-09 20:00:06 +0200

    Data from file now inserts into database. Added notification and summary of import

--- src/main/java/ovh/fejker/filetosql/ImportUsersServlet.java
+++ src/main/java/ovh/fejker/filetosql/ImportUsersServlet.java
@@ -17,22 +17,28 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.*;
 
 @MultipartConfig
 @WebServlet(name = "ImportUsersServlet", value = "/ImportUsersServlet")
 public class ImportUsersServlet extends HttpServlet {
 
+    int result = 0;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html><body><h1>Import successful " + result + " records affected</h1></body></html>");
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            String output = "<html>\n";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java","java", "java");
             Part filePart = request.getPart("file");
             String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
             InputStream fileContent = filePart.getInputStream();
@@ -45,24 +51,35 @@ public class ImportUsersServlet extends HttpServlet {
 
             NodeList list = doc.getElementsByTagName("user");
 
-            output += "<tr>\n";
 
             for(int i = 0; i < list.getLength(); i++) {
                 Node node = list.item(i);
                 if(node.getNodeType() == Node.ELEMENT_NODE) {
                     Element element = (Element) node;
-                    output += "<td>" + element.getElementsByTagName("name").item(0).getTextContent() + "</td>\n";
-                    output += "<td>" + element.getElementsByTagName("surname").item(0).getTextContent() + "</td>\n";
-                    output += "<td>" + element.getElementsByTagName("login").item(0).getTextContent() + "</td>\n";
+                    String name = element.getElementsByTagName("name").item(0).getTextContent();
+                    String surname = element.getElementsByTagName("surname").item(0).getTextContent();
+                    String login = element.getElementsByTagName("login").item(0).getTextContent();
+                    String query = "INSERT INTO users (name, surname, login) VALUES(?,?,?)";
+                    PreparedStatement preparedStatement = connection.prepareStatement(query);
+                    preparedStatement.setString(1, name);
+                    preparedStatement.setString(2, surname);
+                    preparedStatement.setString(3, login);
+                    result += preparedStatement.executeUpdate();
                 }
             }
-            output += "</tr>\n<html>";
-            response.getWriter().write(output);
+
+            connection.close();
 
         } catch (ParserConfigurationException e) {
             throw new RuntimeException(e);
         } catch (SAXException e) {
             throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            doGet(request, response);
         }
 
     }
